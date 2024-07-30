0c80d2582592ffe167f5f7f79009e63c04ba202f
xXcodeXayushXx <codexayush@gmail.com>
2024-07-30 17:08:44 +0530

    Init com

---
+++ src/main/java/io/javabrains/bumble_backend/BumbleBackendApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.bumble_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumbleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumbleBackendApplication.class, args);
+	}
+
+}
